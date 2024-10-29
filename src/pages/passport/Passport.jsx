2112c85dda0d84bfa38e8f53cfb7c9e78dd65292
abdulhayevbahrom <abdulhayevbahromjon2002@gmail.com>
2024-10-29 14:21:28 +0500

    passport added

---
+++ src/pages/passport/Passport.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Section from "../../components/passport/section/Section";
+
+const Passport = () => {
+  return (
+    <div className="passport">
+      <Section />
+    </div>
+  );
+};
+
+export default Passport;
